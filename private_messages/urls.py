b6b845ca6e783ea11309bfa5c5facd7971e017c3
babaroga <mama@mia.com>
2020-07-31 09:52:08 -0400

    send messages inside conversation

--- private_messages/urls.py
+++ private_messages/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('send/', views.send, name='messages-send'),
     path('inbox/', views.inbox, name='messages-inbox'),
     path('conversation/<int:pk>/', views.conversation, name='messages-conversation'),
+    path('converation/start/', views.conversation_start, name='messages-conversation-start'),
     #path('sent/', views.sent, name='messages-sent'),
 ]
--- private_messages/views.py
+++ private_messages/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 from .forms import PrivateMessageForm
 from django.contrib import messages
 from django.conf import settings
@@ -15,7 +16,7 @@ def inbox(request):
     context = {'conversations':conversations}
     return render(request, 'private_messages/inbox.html', context)
 
-# list messages in inbox
+# list messages in specific conversation
 def conversation(request, pk):
     form = PrivateMessageForm()
     conversation = Conversation.objects.get(pk=pk)
@@ -23,25 +24,42 @@ def conversation(request, pk):
     context = {'private_messages':private_messages, 'conversation':conversation, 'form':form}
     return render(request, 'private_messages/conversation.html', context)
 
+def conversation_start(request):
+    if request.user.is_authenticated:
+        ad_id = request.GET['ad']
+        ad = Ad.objects.get(id=ad_id)
+        if request.method == 'POST':
+            form = PrivateMessageForm(request.POST)
+            if form.is_valid():
+                conversation = Conversation.objects.create(ad=ad, starter=request.user)
+                conversation.participants.add(request.user, ad.author)
+                f = form.save(commit=False)
+                f.sender = request.user
+                f.receiver = ad.author
+                f.conversation = conversation
+                f.save()
+                messages.success(request, f'Message sent!')
+                return redirect(request.META['HTTP_REFERER'])
+        else:
+            return redirect('ads-home')
+    else:
+        messages.info(request, f'Please login first')
+        return redirect(f'{settings.LOGIN_URL}?next={request.path}')
+
 
 # form to send private message
 def send(request):
     if request.user.is_authenticated:                
-        ad_id = request.GET['ad']
-        ad = Ad.objects.get(id=ad_id)
         if request.method == 'POST':                       
             form = PrivateMessageForm(request.POST)
             if form.is_valid():
-                conversation = Conversation.objects.create(ad_id=ad_id, starter=request.user)  
-                conversation.participants.add(request.user, ad.author)            
+                conversation = Conversation.objects.get(id=request.GET['conversation'])  
                 f = form.save(commit=False)
                 f.sender = request.user
-                f.receiver = ad.author
-                f.ad_id = ad_id
                 f.conversation = conversation
                 f.save()
                 messages.success(request, f'Message sent!')
-                return redirect('ads-home')
+                return redirect(request.META['HTTP_REFERER'])
         else:
             form = PrivateMessageForm()
             return render(request, 'private_messages/send.html', {'form':form})
