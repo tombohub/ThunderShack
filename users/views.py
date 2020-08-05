4d110280be2185b8fdea4133a035c35f3827f249
babaroga <mama@mia.com>
2020-08-05 17:45:35 -0400

    activate registration email working

--- users/views.py
+++ users/views.py
@@ -1,15 +1,19 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
-from .forms import RegisterForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+
+# email activation
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
-from django.utils.encoding import force_bytes, force_text
-from .tokens import account_activation_token
+from django.utils.encoding import force_bytes, force_str
 from django.core.mail import EmailMessage
 from django.contrib.sites.shortcuts import get_current_site
+
+from .forms import RegisterForm
+from .tokens import account_activation_token
 
 
 def register(request):
@@ -43,6 +47,23 @@ def register(request):
         form = RegisterForm()
     context = {'form': form}
     return render(request, 'users/register.html', context)
+
+
+def activate(request, uidb64, token):
+    uid = force_str(urlsafe_base64_decode(uidb64))
+    try:
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        messages.success(
+            request, f"Thank you {user.username}, you are now logged in!.")
+        return redirect('/')
+    else:
+        return HttpResponse('Activation link is invalid')
 
 
 def user_login(request):
