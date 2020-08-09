78c9750e55cc9b90924a01a0cadb03a1662f0252
babaroga <mama@mia.com>
2020-08-09 02:16:34 -0400

    changed fonts

--- static/js/main.js
+++ static/js/main.js
@@ -12,7 +12,7 @@
 function fillText() {
     const button =  document.getElementById('message1');
     const text =  button.textContent;
-    document.getElementById('id_body').value = text;
+    document.getElementById('ad-reply').value = text;
 }
 
 
