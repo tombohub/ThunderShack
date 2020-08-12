2974cd849c13418bd5dfe2eab25cc72bfb7d06a8
babaroga <mama@mia.com>
2020-08-12 19:58:48 -0400

    added print

--- thunder_shack/settings.py
+++ thunder_shack/settings.py
@@ -17,7 +17,7 @@ from django.contrib.messages import constants as messages
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+print('evo me ovde')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
