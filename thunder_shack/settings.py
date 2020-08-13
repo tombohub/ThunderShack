008aba66e266fe9956e7b32eae48285c8f9350cd
babaroga <mama@mia.com>
2020-08-13 00:56:53 -0400

    commented secret key settings;

--- thunder_shack/settings.py
+++ thunder_shack/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2hc7*o*z)+m)pp+nwnw*iq5lm-yyxv#v^j!7ue04x*jdup=!93'
+# SECRET_KEY = '2hc7*o*z)+m)pp+nwnw*iq5lm-yyxv#v^j!7ue04x*jdup=!93'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
