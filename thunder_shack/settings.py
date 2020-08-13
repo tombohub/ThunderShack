d4e07b1e7d4017f6cd2ff23f241350c2faeac59c
babaroga <mama@mia.com>
2020-08-13 00:48:55 -0400

    uncomment djanggo oh heroku

--- thunder_shack/settings.py
+++ thunder_shack/settings.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
+import django_on_heroku
 from django.contrib.messages import constants as messages
 import os
 print('evo me ovde')
-#import django_on_heroku
 #import django_heroku
 #from .env import *
 
@@ -149,7 +149,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # for Heroku
 # django_heroku.settings(locals())
-# django_on_heroku.settings(locals())
+django_on_heroku.settings(locals())
 
 
 # >> AWS S3 storage
