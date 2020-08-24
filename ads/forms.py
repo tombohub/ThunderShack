3ec020aee519674eed6cdde474ab465597286a62
babaroga <mama@mia.com>
2020-08-24 13:04:02 -0400

    capture attribut

--- ads/forms.py
+++ ads/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 from .models import Ad
 
+
 class AdCreateForm(forms.ModelForm):
+    image = forms.ImageField()
+    image.widget.attrs.update({'capture': ''})
+
     class Meta:
         model = Ad
         fields = ['title', 'price', 'description', 'image']
