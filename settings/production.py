f47da18b579a5bb3d37a95d610f2fae0df9942eb
babaroga <mama@mia.com>
2021-01-04 20:52:41 -0500

    email default change

--- settings/production.py
+++ settings/production.py
@@ -154,7 +154,7 @@ EMAIL_HOST = 'smtp.zoho.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
-DEFAULT_FROM_EMAIL = 'thunder@thundershack.ca'
+DEFAULT_FROM_EMAIL = 'hello@thundershack.ca'
 
 
 # # >> AWS S3 storage
